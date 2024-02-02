ab826da3c59f5fd52185cbe20b67052e1d81d0f9
Usman Butt <usman.butt@outlook.com>
2024-02-02 06:50:05 -0500

    Refactor reducer function and add localStorage saving

--- src/app/exercises/02-checkout/reducer.js
+++ src/app/exercises/02-checkout/reducer.js
@@ -1,12 +1,10 @@
 import { produce } from 'immer';
 
 function reducer(state, action) {
-  return produce(state, (draftState) => {
+  const nextState = produce(state, (draftState) => {
     switch (action.type) {
       case 'add-item': {
-        const itemIndex = state.findIndex(
-          (item) => item.id === action.item.id
-        );
+        const itemIndex = state.findIndex((item) => item.id === action.item.id);
 
         if (itemIndex !== -1) {
           draftState[itemIndex].quantity += 1;
@@ -21,15 +19,23 @@ function reducer(state, action) {
       }
 
       case 'delete-item': {
-        const itemIndex = state.findIndex(
-          (item) => item.id === action.item.id
-        );
+        const itemIndex = state.findIndex((item) => item.id === action.item.id);
 
         draftState.splice(itemIndex, 1);
         return;
       }
+      case 'save-items': {
+        return action.payload;
+      }
     }
   });
+  //Save the state to localStorage
+  // Save the new state to localStorage after every action
+  if (typeof window !== 'undefined' && window.localStorage) {
+    localStorage.setItem('cartItems', JSON.stringify(nextState));
+  }
+
+  return nextState;
 }
 
 export default reducer;
